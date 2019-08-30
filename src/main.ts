c9f3e5484dc3a16467e5e1c915d31bf9cfeea483
Peter Grainger <peter.grainger@sage.com>
2019-08-30 15:15:21 +0100

    add console log

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import * as core from '@actions/core';
 async function run() {
   try {
     const myInput = core.getInput('myInput');
+    console.log('hello!!!')
     core.debug(`Hello ${myInput}`);
   } catch (error) {
+    console.log('failed')
     core.setFailed(error.message);
   }
 }
