01ba1a89d301d36e8920ead39e76e3a42737de67
Peter Grainger <peter.grainger@sage.com>
2019-08-30 15:22:30 +0100

    log context

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import * as core from '@actions/core';
+import * as toolkit from '@actions/github';
 
 async function run() {
   try {
     const myInput = core.getInput('myInput');
-    console.log('hello!!!')
+    const token = core.getInput('token')
+    const octokit = new toolkit.GitHub(token)
+    const context = toolkit.context
+    console.dir(context)
     core.debug(`Hello ${myInput}`);
   } catch (error) {
     console.log('failed')
