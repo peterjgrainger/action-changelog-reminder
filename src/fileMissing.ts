9e4ea880e895a8b02dea8f474cc02f806457a3c5
Fangyi Zhou <me@fangyi.io>
2019-12-05 01:12:49 +0000

    Add option for custom regex

--- src/fileMissing.ts
+++ src/fileMissing.ts
@@ -1,11 +1,15 @@
 import { GitHub } from '@actions/github';
 import { Context } from '@actions/github/lib/context';
 
-export async function fileMissing(octokit: GitHub, actionContext: Context, prNumber: number): Promise<boolean> {
+const default_regex = /change_log\/.*\/*.yml/;
+
+export async function fileMissing(octokit: GitHub, actionContext: Context, prNumber: number, core: any): Promise<boolean> {
+  const regex_str = core.getInput("changelog_regex");
+  const regex = regex_str ? new RegExp(regex_str) : default_regex;
   const files = await octokit.pulls.listFiles({
     ...actionContext.repo,
     pull_number: prNumber
   });
-  const changlelogFiles = files.data.filter(value => /change_log\/.*\/*.yml/.test(value.filename));
+  const changlelogFiles = files.data.filter(value => regex.test(value.filename));
   return changlelogFiles.length === 0;
 }
